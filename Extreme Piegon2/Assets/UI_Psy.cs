d0f51f9230fce91882256a3badf4fb0ec25d210c
Flx <felix.leblancv4si@gmail.com>
2018-07-03 17:32:12 -0400

    Test UI Psy
    J'ai fais quelques test et implémentation dans une scène de test pour le UI de Psy. Apparition, disparition et idle. Utilisation d'une esthétique comics ou BD pour rappeler nos cinématiques.

---
+++ Extreme Piegon2/Assets/UI_Psy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Psy : MonoBehaviour {
+
+	public float Timer = 10f;
+	public bool InterfacePsyIsHere = false;
+
+	public GameObject Psy_UI;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = Psy_UI.GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.tag == "Player") {
+			print ("UI du psy montre toi !");
+			anim.SetBool ("Apparition", true);
+			anim.SetBool ("Disparition", false);
+			InterfacePsyIsHere = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckTimerForPsy ();
+	}
+
+	public void CheckTimerForPsy () {
+		if (InterfacePsyIsHere == true) {
+			Timer = Timer- 1 * Time.deltaTime;
+		}
+
+		if (Timer <= 0) {
+			anim.SetBool ("Disparition", true);
+			anim.SetBool ("Apparition", false);
+		}
+	}
+}
