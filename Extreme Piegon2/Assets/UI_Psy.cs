e67422a5534ec7a1df8e3592a40deb5a1e9842e4
Flx <felix.leblancv4si@gmail.com>
2018-07-04 13:31:12 -0400

    UI Psy Animation terminé
    J'ai fait un prefab dans le dossier UI > Prefab UI de ça et du Trigger qu'il faut avoir (avec le script) pour le déclancher.

--- Extreme Piegon2/Assets/UI_Psy.cs
+++ Extreme Piegon2/Assets/UI_Psy.cs
@@ -1,26 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Psy : MonoBehaviour {
 
-	public float Timer = 10f;
-	public bool InterfacePsyIsHere = false;
+	//Temps avant disparition
+	public float Timer;
+	public bool InterfacePsyIsHere = false; //Pour savoir si le UI du psy est présent ou non
 
-	public GameObject Psy_UI;
-	Animator anim;
+	//public GameObject Psy_UI; //la bulle de texte (AVANT)
+	public GameObject BullePsy; //la bulle de texte (dans le canevas)
+	Animator animBulle;
+	public Image BullePsygeon;
+
+	public GameObject PsyCanvas;
+	Animator animBouton;
+
+	//Texte et bouton à apparaitre
+	public Text TexteDuPsy;
+	public Image BoutonX;
+	public Image BoutonB;
+	public string QuestionPsy;
+	public string ReponsePigeon;
 
 	// Use this for initialization
 	void Start () {
-		anim = Psy_UI.GetComponent<Animator> ();
+		animBulle = BullePsy.GetComponent<Animator> ();
+		animBouton = PsyCanvas.GetComponent<Animator> ();
+		BoutonB.enabled = false;
+		BoutonX.enabled = false;
+		BullePsygeon.enabled = false;
 	}
 
 	void OnTriggerEnter2D (Collider2D other){
 		if (other.gameObject.tag == "Player") {
 			print ("UI du psy montre toi !");
-			anim.SetBool ("Apparition", true);
-			anim.SetBool ("Disparition", false);
+			BullePsygeon.enabled = true;
+			animBulle.SetBool ("Apparition", true);
+			//animBulle.SetBool ("Disparition", false);
 			InterfacePsyIsHere = true;
+
 		}
 	}
 	
@@ -34,9 +54,30 @@ public class UI_Psy : MonoBehaviour {
 			Timer = Timer- 1 * Time.deltaTime;
 		}
 
+		if (Timer <= 28.5) {
+			TexteDuPsy.text = QuestionPsy;
+		}
+
+		if (Timer <= 23 && Timer > 0) {
+			TexteDuPsy.text = "";
+			TexteDuPsy.text = ReponsePigeon;
+			BoutonB.enabled = true;
+			BoutonX.enabled = true;
+		}
+
 		if (Timer <= 0) {
-			anim.SetBool ("Disparition", true);
-			anim.SetBool ("Apparition", false);
+			//animBulle.SetBool ("Disparition", true);
+			//animBulle.SetBool ("Dead", true);
+			//animBulle.SetBool ("Apparition", false);
+			animBouton.SetBool ("Disparition",true);
+//			BoutonB.enabled = false;
+//			BoutonX.enabled = false;
+			InterfacePsyIsHere = false;
+		}
+
+		if (InterfacePsyIsHere == false) {
+			//animBouton.SetBool ("Disparition", false);
+			TexteDuPsy.text = "";
 		}
 	}
 }
