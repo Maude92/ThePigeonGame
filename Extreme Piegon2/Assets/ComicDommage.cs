257bfe839a14ee491d4b5a6a89f96531a46bc08a
Flx <felix.leblancv4si@gmail.com>
2018-07-04 15:40:00 -0400

    Mise en place des Commix Dammage
    Fonctionne pour l'instant en OnTriggerEnter2D et OnCollisionEnter2D

---
+++ Extreme Piegon2/Assets/ComicDommage.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComicDommage : MonoBehaviour {
+
+	public GameObject[] ImgDammage;
+	int n;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+		if (col.gameObject.tag != "Player") {
+			n = Random.Range (1, 4);
+			DammageCheck ();
+		}
+			
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.tag != "Player") {
+			n = Random.Range (1, 4);
+			DammageCheck ();
+		}
+	}
+
+	public void DammageCheck (){
+		if (n == 1) {
+			ImgDammage [0].SetActive (true);
+			ImgDammage [1].SetActive (false);
+			ImgDammage [2].SetActive (false);
+		}
+
+		if (n == 2){
+			ImgDammage [0].SetActive (false);
+			ImgDammage [1].SetActive (true);
+			ImgDammage [2].SetActive (false);
+		}
+
+		if (n == 3) {
+			ImgDammage [0].SetActive (false);
+			ImgDammage [1].SetActive (false);
+			ImgDammage [2].SetActive (true);
+		}
+	}
+}
