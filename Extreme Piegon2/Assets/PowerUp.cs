c1a3da45b0014884ec29c3269bb74264a41d493e
Flx <felix.leblancv4si@gmail.com>
2018-07-09 11:23:08 -0400

    PowerUp UI mis en place
    On va tester cela Maude !

---
+++ Extreme Piegon2/Assets/PowerUp.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUp : MonoBehaviour {
+
+	//Scream of Death --> = 1
+	public GameObject PowerUp_SoDWorld;
+	public Image PowerUp_SoDUI;
+
+	//Invincibilité --> = 2
+	public GameObject PowerUp_InvincibilityWorld;
+	public Image PowerUp_InvicibilityUI;
+
+	public bool BeakyGotAPowerUp;
+	public int WichPowerUp;
+
+	void Start (){
+		PowerUp_SoDUI.enabled = false;
+		PowerUp_InvicibilityUI.enabled = false;
+		BeakyGotAPowerUp = false;
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.tag == "Player" && BeakyGotAPowerUp == false) {
+			BeakyGotAPowerUp = true; //à accéder avec le script de contrôle pour activer le PowerUp
+			PowerUpCheck ();
+		}
+	}
+
+	public void PowerUpCheck(){
+
+		//Désactiver les PowerUp dans la box lorsque vide
+		if (WichPowerUp == 0) {
+			PowerUp_SoDUI.enabled = false;
+			PowerUp_InvicibilityUI.enabled = false;
+		}
+
+		//Scream of Death
+		if (WichPowerUp == 1) {
+			//Désactiver le PowerUp du World et activer le PowerUp dans la box
+			PowerUp_SoDWorld.SetActive (false); //faire disparaitre le powerup dans le monde
+			PowerUp_SoDUI.enabled = true; //mettre powerup dans la box
+
+			//Désactiver les autres dans la box
+			PowerUp_InvicibilityUI.enabled = false;
+			//... autre s'il y a lieu
+		}
+
+		//Invincibility
+		if (WichPowerUp == 2) {
+			PowerUp_InvincibilityWorld.SetActive (false);
+			PowerUp_InvicibilityUI.enabled = true;
+
+			PowerUp_SoDUI.enabled = false;
+		}
+	}
+}
