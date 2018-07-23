0d556d5f3a6a0d8ee3f919cd55d0f02e4897ef43
Flx <felix.leblancv4si@gmail.com>
2018-07-23 18:27:39 -0400

    Onomatopes fonctionnels
    Onomatopé fonctionnels en jeu, animation faite+ code mis à jour pour fonctionner avec les 7.

--- Extreme Piegon2/Assets/UI/Script_UI/ComicDommage.cs
+++ Extreme Piegon2/Assets/UI/Script_UI/ComicDommage.cs
@@ -10,7 +10,7 @@ public class ComicDommage : MonoBehaviour {
 	//Changer le != "Player" par un Tag Décor pour les éléments du décor"
 	void OnCollisionEnter2D (Collision2D col){
 		if (col.gameObject.tag != "Player") {
-			NumberOfImages = Random.Range (1, 4);
+			NumberOfImages = Random.Range (1, 8);
 			DammageCheck ();
 		}
 			
@@ -18,7 +18,7 @@ public class ComicDommage : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other){
 		if (other.gameObject.tag == "Ennemi") {
-			NumberOfImages = Random.Range (1, 4);
+			NumberOfImages = Random.Range (1, 8);
 			DammageCheck ();
 		}
 	}
@@ -28,18 +28,70 @@ public class ComicDommage : MonoBehaviour {
 			ImgDammage [0].SetActive (true);
 			ImgDammage [1].SetActive (false);
 			ImgDammage [2].SetActive (false);
+			ImgDammage [3].SetActive (false);
+			ImgDammage [4].SetActive (false);
+			ImgDammage [5].SetActive (false);
+			ImgDammage [6].SetActive (false);
 		}
 
 		if (NumberOfImages == 2){
 			ImgDammage [0].SetActive (false);
 			ImgDammage [1].SetActive (true);
 			ImgDammage [2].SetActive (false);
+			ImgDammage [3].SetActive (false);
+			ImgDammage [4].SetActive (false);
+			ImgDammage [5].SetActive (false);
+			ImgDammage [6].SetActive (false);
 		}
 
 		if (NumberOfImages == 3) {
 			ImgDammage [0].SetActive (false);
 			ImgDammage [1].SetActive (false);
 			ImgDammage [2].SetActive (true);
+			ImgDammage [3].SetActive (false);
+			ImgDammage [4].SetActive (false);
+			ImgDammage [5].SetActive (false);
+			ImgDammage [6].SetActive (false);
+		}
+
+		if (NumberOfImages == 4) {
+			ImgDammage [0].SetActive (false);
+			ImgDammage [1].SetActive (false);
+			ImgDammage [2].SetActive (false);
+			ImgDammage [3].SetActive (true);
+			ImgDammage [4].SetActive (false);
+			ImgDammage [5].SetActive (false);
+			ImgDammage [6].SetActive (false);
+		}
+
+		if (NumberOfImages == 5) {
+			ImgDammage [0].SetActive (false);
+			ImgDammage [1].SetActive (false);
+			ImgDammage [2].SetActive (false);
+			ImgDammage [3].SetActive (false);
+			ImgDammage [4].SetActive (true);
+			ImgDammage [5].SetActive (false);
+			ImgDammage [6].SetActive (false);
+		}
+
+		if (NumberOfImages == 6) {
+			ImgDammage [0].SetActive (false);
+			ImgDammage [1].SetActive (false);
+			ImgDammage [2].SetActive (false);
+			ImgDammage [3].SetActive (false);
+			ImgDammage [4].SetActive (false);
+			ImgDammage [5].SetActive (true);
+			ImgDammage [6].SetActive (false);
+		}
+
+		if (NumberOfImages == 7) {
+			ImgDammage [0].SetActive (false);
+			ImgDammage [1].SetActive (false);
+			ImgDammage [2].SetActive (false);
+			ImgDammage [3].SetActive (false);
+			ImgDammage [4].SetActive (false);
+			ImgDammage [5].SetActive (false);
+			ImgDammage [6].SetActive (true);
 		}
 	}
 }
