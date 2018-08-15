a15fe45387f6be6a9a2da6e97802f54089c77c74
Maude92 <36176462+Maude92@users.noreply.github.com>
2018-08-15 11:31:38 -0400

    RollingBoulder
    Rien qui fonctionne pour l'instant...

---
+++ Extreme Piegon2/Assets/MoveThatBoulder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveThatBoulder : MonoBehaviour {
+
+	public float boulderspeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D (Collider2D other){
+		if (other.gameObject.tag == "Boulder") {
+			print ("La roche touche le sol!");
+			Rigidbody2D rbboulder;
+			rbboulder = other.GetComponent<Rigidbody2D> ();
+			rbboulder.AddForce (new Vector2 (boulderspeed, 0));
+		}
+	}
+}
--- Extreme Piegon2/Assets/Script/GoFaster.cs
+++ Extreme Piegon2/Assets/Script/GoFaster.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class GoFaster : MonoBehaviour {
 
 	public GameObject laCamera;
-	public float newSpeed = 3f;
+	public float newSpeed = 4.5f;
+	public float newDownSpeed = 1.75f;
 
 	MoveCameraNEW movecameranewscript;
 
@@ -17,12 +18,14 @@ public class GoFaster : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other){
 		if (other.gameObject.tag == "Player") {
 			movecameranewscript.speed = newSpeed;
+		//	movecameranewscript.downSpeed = newDownSpeed;
 		}
 	}
 
 	void OnTriggerExit2D (Collider2D other){
 		if (other.gameObject.tag == "Player") {
 			movecameranewscript.speed = 2.5f;
+		//	movecameranewscript.downSpeed = 0.25f;
 		}
 	}
 }
